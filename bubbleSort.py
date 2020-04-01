bca88bedcc4c0bf82672271669ebd73cc0d6786c
Jacques Fidelia Jr <jacques.fidelia@gmail.com>
2020-04-01 22:01:26 +0000

    first commit

---
+++ bubbleSort.py
@@ -0,0 +1,9 @@
+def bs(a):                   # a = name of list
+    b=len(a)-1
+    for x in range(b):
+        for y in range(b-x):
+            if a[y]>a[y+1]:
+                a[y],a[y+1]=a[y+1],a[y]
+    return a
+a=[3,6,1,8]
+print(bs(a))
